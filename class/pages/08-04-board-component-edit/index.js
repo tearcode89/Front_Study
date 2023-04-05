fa6b502a188178c2b624fc35099e7ad59e13cdb6
sjun8932.3 <sjun8932.3@gmail.com>
2023-04-05 16:53:56 +0900

    22. 컴포넌트 재사용성

---
+++ class/pages/08-04-board-component-edit/index.js
@@ -0,0 +1,8 @@
+import BoardComponent from "../../src/components/units/board/08-board-component";
+
+export default function BoardEditPage() {
+
+    return(
+        <BoardComponent isEdit={true}/>
+    )
+}
---
+++ class/pages/08-05-boards/[CustomIndex]/edit/index.js
@@ -0,0 +1,13 @@
+import BoardWrite from "../../../../src/components/units/board/08-write/BoardWrite.container";
+
+export default function GraphqlMutationPage() {
+
+    // 자바스크립트 영역
+
+    // HTML영역 (return 아래)
+    return(
+        <>
+            <BoardWrite/>
+        </>
+    )
+}
